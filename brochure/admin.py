d82987778a4cd2791489eea8c8639c7cf7d1ed71
Alan Chan <achan961117@gmail.com>
2018-07-16 05:48:47 +0100

    Add content and meta to admin and template

--- brochure/admin.py
+++ brochure/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Speaker
+from .models import Speaker, Metatag, Content
 
 # Register your models here.
 admin.site.register(Speaker)
+admin.site.register(Metatag)
+admin.site.register(Content)

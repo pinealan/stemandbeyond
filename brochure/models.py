953643f2b8b49d411296dab01b3fa1058ad67cc6
Alan Chan <achan961117@gmail.com>
2018-07-19 01:50:03 +0100

    Add "link" field to brochure affiliate model

--- brochure/models.py
+++ brochure/models.py
@@ -50,6 +50,7 @@ class Content(Model):
 
 class Affiliate(Model):
     name = models.CharField(max_length=100)
+    link = models.CharField(max_length=200, blank=True)
     photo = models.ImageField(
         upload_to=format_photo_path
     )
